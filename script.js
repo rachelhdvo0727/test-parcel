554ec95cbdd130cb4009a994a732655ddcaa9edf
rachelvo <rach0727@stud.kea.dk>
2020-03-16 17:25:30 +0100

    done static-files

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-document.querySelector("h1").textContent = "I got a parcel";
+//document.querySelector("h1").textContent = "I got a parcel";
+const data = [
+  {
+    i: "half-blood.jpg"
+  },
+  {
+    i: "hallows_1.jpg"
+  }
+];
+data.forEach(d => {
+  const t = document.createElement("img");
+  t.src = "images/" + d.i;
+  document.body.appendChild(t);
+});
